bb7ded68919dd3ac03454fbe1dfc1f4fd0ae7762
COBIECODES <dibia3527@gmail.com>
2022-12-13 14:55:17 -0800

    task 11 again

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers to 98
+ * print_to_98 - prints all natural numbers available from
+ * n to 98, separated by comma and space. (in order)
  *
- * @n: number provided by user
+ * @n: input number
  *
  * Return void
  */
@@ -13,11 +14,11 @@ void print_to_98(int n)
 {
 	int num;
 
-	if (n < 98)
-		for (num = n; num < 98; --num)
+	if (n > 98)
+		for (num = n; num > 98; --num)
 			printf("%d, ", num);
 	else
-		for (num = n; num > 98; ++num)
+		for (num = n; num < 98; ++num)
 			printf("%d, ", num);
 	printf("98\n");
 }
