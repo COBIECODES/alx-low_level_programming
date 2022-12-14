e467ae5aa139b6c572522927bb1469969c9a2da1
COBIECODES <dibia3527@gmail.com>
2022-12-14 12:37:26 -0800

    tasks

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,7 +3,7 @@
 
 /**
  * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
+i * left in the year, taking leap years into account
  * @month: month in number format
  * @day: day of month
  * @year: year
@@ -18,7 +18,6 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++;
 		}
-
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
