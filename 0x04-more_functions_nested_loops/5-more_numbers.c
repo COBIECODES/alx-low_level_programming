391ee0a104fafe48198952c536a85cdbb2869949
COBIECODES <dibia3527@gmail.com>
2022-12-15 14:18:59 -0800

    task update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,24 @@
 #include "main.h"
+
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
- * Description: prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  * Return: 0
  */
 
 void more_numbers(void)
 
 {
-	int i, j;
+	int i, j = 0;
 
-	for (i = 0; i < 10; i++)
+	for (; j < 10; j++)
 	{
-		for (j = 0; j < 15; j++)
+		for (i = 0; i <=14; i++)
 		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			if (i > 9)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 		}
-		_putchar('\n);
+		_putchar('\n');
 	}
 }
