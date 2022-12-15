41976b9fe22f1ee56312520eff5a0e6888933486
COBIECODES <dibia3527@gmail.com>
2022-12-15 13:35:38 -0800

    tasks update

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(0)
+{
+	print triangl(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	return (0);
+}
