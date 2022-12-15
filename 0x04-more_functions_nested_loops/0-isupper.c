c8c7a135f82545f330d8165666e254ecc5a3560c
COBIECODES <dibia3527@gmail.com>
2022-12-15 06:04:30 -0800

    task 1 update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,19 @@
  * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
-	if ((c >= 'A') && (C <= '2'))
-		return (1);
-	else
-		return (0);
-}
+	char uppercase = 'A';
+	int isupper = '0';
+
+	for (; uppercase <= 'Z'; uppercase++)
+
+	{
+		if (c == uppercase)
+		{
+			isupper = 1;
+			break;
+		}
+		return (isupper);
+	}
