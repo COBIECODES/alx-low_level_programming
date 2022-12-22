a2feb643cf097968bf250a9ae002442631303d3d
COBIECODES <dibia3527@gmail.com>
2022-12-22 14:51:13 -0800

    tasks

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * print_buffer - prints a buffer 10 bytes at a time, starting with
- * the byte position, then showing the hex content,
- * then displaying printable characters.
+ * print_buffer - Prints a buffer 10 bytes at a time, starting with
+ *                the byte position, then showing the hex content,
+ *                then displaying printable charcaters.
  * @b: The buffer to be printed.
  * @size: The number of bytes to be printed from the buffer.
  */
@@ -20,7 +20,7 @@ void print_buffer(char *b, int size)
 		for (index = 0; index < 10; index++)
 		{
 			if ((index + byte) >= size)
-				printf(" ");
+				printf("  ");
 			else
 				printf("%02x", *(b + index + byte));
 			if ((index % 2) != 0 && index != 0)
@@ -31,14 +31,13 @@ void print_buffer(char *b, int size)
 			if ((index + byte) >= size)
 				break;
 			else if (*(b + index + byte) >= 31 &&
-					*(b + index + byte) <= 126)
+				 *(b + index + byte) <= 126)
 				printf("%c", *(b + index + byte));
 			else
 				printf(".");
 		}
 		if (byte >= size)
 			continue;
-
 		printf("\n");
 	}
 	if (size <= 0)
