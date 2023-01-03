3d4a33cb057d75756b2d9995780daebca3a04248
COBIECODES <dibia3527@gmail.com>
2023-01-02 17:17:49 -0800

    tasks

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,22 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
- * @s: string
- * @c: character to search
- * Return: Returns a pointer to the first occurrence of the character c in the string s
+ * _strchr - locate character in a string
+ * @s: char array string
+ * @c: char to look for
+ * Return: NULL if char not found, or pointer to first occurrence of char 'c'
  */
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0') /*Declaring WHILE*/
+	while (*s != '\0')
 	{
 		if (*s == c)
-		{
 			return (s);
-		}
-		++s;
+		else if (*(s + 1) == c)
+			return (s + 1);
+		s++;
 	}
 
-	if (*s == c)
-	{
-		return (s);
-	}
-
-	return (0); /*values null*/
+	return (s + 1);
 }
